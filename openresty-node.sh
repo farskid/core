b79590fef19e21ac3fe9f9df88aa25bc88c39d04
Majid Taheri(I love optimization) <taheri24@users.noreply.github.com>
2016-10-31 00:40:26 +0330

    Update openresty-node.sh

--- openresty-node.sh
+++ openresty-node.sh
@@ -6,7 +6,7 @@ if [ -f /etc/redhat-release ]; then
   yum -y update
   yum -y install epel-release
   yum install gcc-c++
-  yum  -y install nginx-extras build-essential libpcre3-dev libssl-dev libgeoip-dev libpq-dev libxslt1-dev libgd2-xpm-dev libdrizzle-dev
+  yum  -y install nginx-extras build-essential libpcre3-devel libssl-devel libgeoip-devel libxslt1-devel libgd2-xpm-devel libdrizzle-devel
 fi
  
 if [ -f /etc/lsb-release ]; then
