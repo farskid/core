0ae98f82200f47c77c5bb9bf5c5479243c02d32f
Majid Taheri(I love optimization) <taheri24@users.noreply.github.com>
2016-11-20 13:43:45 +0330

    Update gitlab-centos.sh

--- gitlab-centos.sh
+++ gitlab-centos.sh
@@ -7,7 +7,7 @@ rpm -Uvh http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.r
 yum -y groupinstall 'Development Tools' 'Additional Development'
 yum -y install readline readline-devel ncurses-devel gdbm-devel glibc-devel tcl-devel openssl-devel curl-devel expat-devel db4-devel byacc gitolite sqlite-devel gcc-c++ libyaml libyaml-devel libffi libffi-devel libxml2 libxml2-devel libxslt libxslt-devel libicu libicu-devel system-config-firewall-tui python-devel
 
-curl -O "http://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.3-p0.tar.gz"
+wget "http://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.3-p0.tar.gz"
 tar xzvf ruby-1.9.3-p0.tar.gz
 cd ruby-1.9.3-p0
 ./configure --enable-shared --disable-pthread
